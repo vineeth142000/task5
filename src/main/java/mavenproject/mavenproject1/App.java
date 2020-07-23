c860725211820b0dcf41dece2b7766339ec3f669
vineeth142000 <60319646+vineeth142000@users.noreply.github.com>
2020-07-23 15:57:36 +0530

    Add files via upload

---
+++ src/main/java/mavenproject/mavenproject1/App.java
@@ -0,0 +1,13 @@
+package mavenproject.mavenproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
